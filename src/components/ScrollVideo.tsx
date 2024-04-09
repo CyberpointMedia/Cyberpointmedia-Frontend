a283735af99c78b2a5cc4a66adee022339ec8cda
mukeshkumar1988 <mukesh@cyberpointmedia.com>
2024-04-09 21:10:53 +0530

    video animation zoom effects on scroll

--- src/components/ScrollVideo.tsx
+++ src/components/ScrollVideo.tsx
@@ -1,5 +1,6 @@
 import styles from '@/styles/CustomCursor.module.css';
-// import video1 from '../../public/videos/video.mp4'; // Path to your video file
+// @ts-ignore
+import video1 from '../../public/videos/video.mp4'; // Path to your video file
 import { useScroll, useTransform, motion } from 'framer-motion';
 import { useRef } from 'react';
 
@@ -11,7 +12,7 @@ export default function ScrollVideo() {
         offset: ['start start', 'end end']
     })
 
-    const scale4 = useTransform(scrollYProgress, [0, 1], [1, 4]);
+    const scale4 = useTransform(scrollYProgress, [0, 1], [1, 100]);
 
     const scrollEffect = [
         {
@@ -20,66 +21,21 @@ export default function ScrollVideo() {
     ]
  
     return (<>
-        <div className="relative">
-            <div className="">
-                <div  className={styles.heroSection}>
-                    <div className={styles.sticky}>
-                        {
-                            scrollEffect.map( ({scale}, index) => {
-                                return <motion.div key={index} style={{scale}} className={styles.el}>
-                                    <div className={styles.videoContainer}>
+        <div className={styles.videoWrp}>
+            {
+                scrollEffect.map( ({scale}, index) => {
+                    return <motion.div key={index} style={{scale}} className={styles.videoInnerWrp}>
+                                <div className={styles.videoContainer}>
+                                    <div className={styles.aaa}>
                                         <video autoPlay loop muted className={styles.video}>
-                                            <source  type="video/mp4" />
+                                            <source src={video1}  type="video/mp4" />
                                         </video>
                                     </div>
-                                </motion.div>
-                            })
-                        }
-                    </div>
-                </div>
-            </div>
+                                </div>
+                            </motion.div>
+                })
+            }
         </div>
-        <section className={styles.hFull}>
-            <div>
-                <h1 className={styles.white}>SECTION 2 START HERE </h1>
-                <p className={styles.white}>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                </p>
-                <br /><br />
-                <p className={styles.white}>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                </p>
-                <br /><br />
-                <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                </p>
-                <br /><br />
-                <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                </p>
-                <br /><br />
-                <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                </p>
-                <br /><br />
-                <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                </p>
-                <br /><br />
-                <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                </p>
-                <br /><br />
-                <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                </p>
-                <br /><br />
-                <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                </p>
-                <br /><br />
-            </div>
-        </section>
         </>
     )
 }
